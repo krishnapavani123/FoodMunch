27fa9245d57555f6d2e10a3692536476ddb32bf5
pavani <krishnapavani909@gmail.com>
2025-06-18 15:32:33 +0530

    foodmunch

---
+++ script.js
@@ -0,0 +1,10 @@
+document.getElementById("viewMenuButton").addEventListener("click", function() {
+    window.location.href = "menue.html"; });
+    document.getElementById("back").addEventListener("click", function() {
+    window.location.href = "Home.html"; });
+    document.getElementById("non-v").addEventListener("click", function() {
+    window.location.href = "non-veg.html"; });
+    document.getElementById("back").addEventListener("click", function() {
+    window.location.href = "Home.html"; });
+        document.getElementById("back").addEventListener("click", function() {
+    window.location.href = "Home.html"; });
